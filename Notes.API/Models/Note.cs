384b0a47c21eae02111bacda365ed971a966d2eb
Pawel Gandorski <pgandorski@gmail.com>
2020-09-07 15:44:19 +0200

    Add project files.

---
+++ Notes.API/Models/Note.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Notes.API.Models
+{
+    public class Note
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Content { get; set; }
+
+        [Required]
+        public DateTime AddDate { get; set; }
+    }
+}
---
+++ Notes.API/Models/NoteContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Threading.Tasks;
+
+namespace Notes.API.Models
+{
+    public class NoteContext : DbContext
+    {
+        public NoteContext(DbContextOptions<NoteContext> options) 
+            : base(options)
+        { }   
+
+        public DbSet<Note> Notes { get; set; }
+    }
+}
